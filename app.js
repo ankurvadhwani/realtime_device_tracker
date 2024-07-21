54f54b0dae5b69f3a8c666ae72489930a2d6bb96
Ankur Vadhwani <www.nankur113@gmail.com>
2024-07-21 17:48:31 +0530

    complete code

---
+++ app.js
@@ -0,0 +1,35 @@
+const express=require('express');
+const app=express();
+const http=require('http')
+const path=require("path");
+const socketio= require('socket.io');
+
+const server =http.createServer(app);
+
+const io=socketio(server);
+
+
+app.set("view engine","ejs")
+app.use(express.static(path.join(__dirname,"public")));
+
+io.on('connection',function(socket){
+    socket.on("send-location",(data)=>{
+        io.emit("receive-location",{id:socket.id,...data});
+    })
+
+    socket.on("disconnect",()=>{
+        io.emit("user-disconnected",socket.id);
+    })
+    console.log("connected");
+})
+
+app.use((req, res, next) => {
+    console.log(`Request URL: ${req.url}`);
+    next();
+});
+
+app.get('/',function (req,res){
+    res.render('index');
+});
+
+server.listen(3000)
